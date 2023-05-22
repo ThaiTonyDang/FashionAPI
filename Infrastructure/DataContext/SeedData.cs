92acdb86e06a1785a776b21c69fdaf64b259bea1
Thuy Dang Tran <thuy.dang@plenti.com.au>
2023-05-22 13:58:35 +0700

    FT-470: Seeding User Role

---
+++ Infrastructure/DataContext/SeedData.cs
@@ -0,0 +1,82 @@
+﻿using Infrastructure.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DataContext
+{
+    public class SeedData
+    {
+        public static async Task InitializeUserAndRole(AppDbContext context, IServiceProvider servicesProvider)
+        {
+            using (var scope = servicesProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                context.Database.Migrate();
+
+                var userMgr = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+                var roleMgr = scope.ServiceProvider.GetRequiredService<RoleManager<Role>>();
+
+                // created admin role if not existed
+                var isAdminRoleExisted = await roleMgr.RoleExistsAsync("Admin");
+                if (!isAdminRoleExisted)
+                {
+                    await roleMgr.CreateAsync(new Role("Admin"));
+                }
+
+                // create user admin if not existed
+                var adminEmail = "admin@gmail.com";
+                var admin = await userMgr.FindByEmailAsync(adminEmail);
+                if(admin == null)
+                {
+                    var adminUser = new User
+                    {
+                        Email = "admin@gmail.com",
+                        UserName = "admin@gmail.com",
+                        FirstName = "admin",
+                        LastName = "admin",
+                    };
+
+                    var result = await userMgr.CreateAsync(adminUser, "abcd@1234");
+                    if (result.Succeeded)
+                        admin = adminUser;
+                }
+
+                var isAdminRole = await userMgr.IsInRoleAsync(admin, "Admin");
+                if (!isAdminRole)
+                {
+                    await userMgr.AddToRoleAsync(admin, "Admin");
+                }
+
+                var isUserRoleExisted = await roleMgr.RoleExistsAsync("User");
+                if (!isAdminRoleExisted)
+                {
+                    await roleMgr.CreateAsync(new Role("User"));
+                }
+
+                // create user admin if not existed
+                var deafaultUserEmail = "user@gmail.com";
+                var defaultUser = await userMgr.FindByEmailAsync(deafaultUserEmail);
+                if (defaultUser == null)
+                {
+                    var user = new User
+                    {
+                        Email = "admin@gmail.com",
+                        UserName = "admin@gmail.com",
+                        FirstName = "admin",
+                        LastName = "admin",
+                    };
+
+                    var result = await userMgr.CreateAsync(user, "abcd@1234");
+                    if (result.Succeeded)
+                        defaultUser = user;
+                }
+
+                var isUserRole = await userMgr.IsInRoleAsync(defaultUser, "User");
+                if (!isUserRole)
+                {
+                    await userMgr.AddToRoleAsync(defaultUser, "User");
+                }
+            }
+        }
+    }
+}
--- Infrastructure/Models/Role.cs
+++ Infrastructure/Models/Role.cs
@@ -4,6 +4,15 @@ namespace Infrastructure.Models
 {
     public class Role : IdentityRole<Guid>
     {
+        public Role()
+        {
+
+        }
+        public Role(string role)
+        {
+            this.Name = role;
+        }
+
         public DateTime CreatedDate { get; set; }
         public virtual ICollection<UserRole> UserRoles { get; set; }
     }
