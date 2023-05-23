00eaac9b612c66dc0d32afccdb8de44a18ded5ee
Tommy Dang <dtranthuy1995@gmail.com>
2023-05-23 22:30:57 +0700

    refactor response

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
-﻿using Domain.Dtos;
+﻿using API.Dtos;
+using Domain.Dtos;
 using Domain.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Threading.Tasks;
@@ -16,60 +18,98 @@ namespace API.Controllers
             this._userService = userService;
         }
 
+        [AllowAnonymous]
         [HttpPost]
+        [Route("register")]
         public async Task<IActionResult> Register([FromBody] UserRegistrationDto user)
         {
             if (user == null)
             {
-                return BadRequest("User can not be null");
+                return BadRequest(new Error<string>(
+                        (int)HttpStatusCode.BadRequest,
+                        "Register Failed",
+                        "User can not be null")
+                    );
             }
 
             if (string.IsNullOrEmpty(user.Email))
             {
-                return BadRequest("Email can not be null or empty");
+                return BadRequest(new Error<string>(
+                        (int)HttpStatusCode.BadRequest,
+                        "Register Failed",
+                        "Email can not be null or empty")
+                    );
             }
 
             if (!user.Password.Equals(user.ConfirmPassword))
             {
-                return BadRequest("Password confirm is not match");
+                return BadRequest(new Error<string>(
+                        (int)HttpStatusCode.BadRequest,
+                        "Register Failed",
+                        "Password confirm is not match")
+                    );
             }
 
             var result = await this._userService.RegisterUserAsync(user);
             if (!result)
             {
-                return StatusCode((int)HttpStatusCode.InternalServerError, new
-                {
-                    message = "Can't create new user"
-                });
+                return BadRequest(new Error<string>(
+                        (int)HttpStatusCode.InternalServerError,
+                        "Register Failed",
+                        "Can't create new user")
+                    );
             }
-
-            return StatusCode((int)HttpStatusCode.Created);
+            var response = new Success((int)HttpStatusCode.Created, "Created new user");
+            return Ok(response);
         }
 
-        [HttpPost("login")]
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("login")]
         public async Task<IActionResult> Login([FromBody] UserDto user)
         {
             if (user == null)
             {
-                return BadRequest("User can not be null");
+                return BadRequest(new Error<string>(
+                        (int)HttpStatusCode.BadRequest,
+                        "Login Failed",
+                        "User can not be null or empty")
+                    );
             }
             
             if(string.IsNullOrEmpty(user.Email))
             {
-                return BadRequest("Email can not be null or empty");
+                return BadRequest(new Error<string>(
+                        (int)HttpStatusCode.BadRequest,
+                        "Login Failed",
+                        "Email can not be null or empty")
+                    );
             }
 
             if (!user.Password.Equals(user.Password))
             {
-                return BadRequest("Password can not be null or empty");
+                return BadRequest(new Error<string>(
+                        (int)HttpStatusCode.BadRequest,
+                        "Login Failed",
+                        "Password can not be null or empty")
+                    );
             }
 
             var isValidated = await this._userService.ValidateUserAsync(user);
             if(!isValidated)
-                return Unauthorized("Username or Password are not correct");
+                return Unauthorized(new Error<string>(
+                        (int)HttpStatusCode.BadRequest,
+                        "Login Failed",
+                        "Username or Password are not correct")
+                    );
 
             var token = await this._userService.CreateTokenAsync(user);
-            return Ok(token);
+            var response = new SuccessData<string>(
+                    (int)HttpStatusCode.OK,
+                    "Login Successufully",
+                    token
+                );
+            return Ok(response);
         }
     }
 }
